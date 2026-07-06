95276c3591fff188a6e31142a87a5df196c87216
Nasruddin <jundi216@gmail.com>
2026-07-06 07:51:37 +0700

    Simulasi kegagalan build untuk demonstrasi pipeline (percobaan 2)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import BrokenImport from "./file-yang-benar-benar-tidak-ada";
 
 createRoot(document.getElementById("root")!).render(<App />);
